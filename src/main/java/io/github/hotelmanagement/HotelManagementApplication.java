4db5006472add7024a748a10fe39aba3766f64e3
Mikołaj <mikoturko@wp.pl>
2024-03-15 18:34:33 +0100

    first commit

---
+++ src/main/java/io/github/hotelmanagement/HotelManagementApplication.java
@@ -0,0 +1,13 @@
+package io.github.hotelmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagementApplication.class, args);
+	}
+
+}
