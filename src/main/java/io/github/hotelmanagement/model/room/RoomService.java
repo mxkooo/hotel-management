ae347cc47f43ceb85f3ce9b149863a00f77c6340
Mikołaj <mikoturko@wp.pl>
2024-03-16 20:05:26 +0100

    Tried to create a method creating room

--- src/main/java/io/github/hotelmanagement/model/room/RoomService.java
+++ src/main/java/io/github/hotelmanagement/model/room/RoomService.java
@@ -1,8 +1,16 @@
 package io.github.hotelmanagement.model.room;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RoomService {
+
+    public ResponseEntity<Room> createRoom(){
+
+
+
+        return ResponseEntity.ok(Room.builder().build());
+    }
 
 }
