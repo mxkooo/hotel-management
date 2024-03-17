d5bb93158341ea3daadccae78a34274cd1afcf8f
Mikołaj <mikoturko@wp.pl>
2024-03-17 13:56:20 +0100

    Added little changes in Price class.

--- src/main/java/io/github/hotelmanagement/model/price/Price.java
+++ src/main/java/io/github/hotelmanagement/model/price/Price.java
@@ -1,9 +1,14 @@
 package io.github.hotelmanagement.model.price;
 
+import io.github.hotelmanagement.model.room.RoomMapper;
+import io.github.hotelmanagement.model.room.RoomRepository;
 import lombok.ToString;
 
 @ToString
 public class Price {
+    private RoomMapper mapper;
+    private RoomRepository roomRepository;
+
     public int countPrice(int beds){
         // 1 bed - 80PLN
         int price = beds * 80;
