7a0035b44ac5cda23df82d39edbb5a9c0a312fff
PatykKon <115705372+PatykKon@users.noreply.github.com>
2024-03-20 16:09:27 +0100

    [HM] | feature-room-controller -update room controller

--- src/main/java/io/github/hotelmanagement/model/room/RoomController.java
+++ src/main/java/io/github/hotelmanagement/model/room/RoomController.java
@@ -1,7 +1,21 @@
 package io.github.hotelmanagement.model.room;
 
+import io.github.hotelmanagement.model.user.UserDTO;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
+@RequestMapping("/api")
 public class RoomController {
+
+    RoomService roomService;
+
+    @PostMapping
+    RoomDTO crateRoom(@RequestBody RoomDTO roomDTO){
+       return roomService.createRoom(roomDTO);
+    }
 }
