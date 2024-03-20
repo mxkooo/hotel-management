1f394ee7b37d2dfb9939fe578ea192adfd6d861f
PatykKon <115705372+PatykKon@users.noreply.github.com>
2024-03-20 09:54:30 +0100

    [HM] | feature-check-availability-room-in-range-time (#28)
    -add refactor

--- src/main/java/io/github/hotelmanagement/model/room/RoomServiceImpl.java
+++ src/main/java/io/github/hotelmanagement/model/room/RoomServiceImpl.java
@@ -1,6 +1,7 @@
 package io.github.hotelmanagement.model.room;
 
 import io.github.hotelmanagement.model.price.Price;
+import io.github.hotelmanagement.model.reservation.Reservation;
 import io.github.hotelmanagement.model.room.exception.GetAvailableRoomException;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
@@ -8,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 @Service
@@ -31,17 +33,16 @@ class RoomServiceImpl implements RoomService {
 
     public RoomDTO getAvailableRoom(final LocalDateTime startDate, final LocalDateTime endDate, final int bedAmount) {
 
-        if (startDate == null) {
-            throw new IllegalArgumentException("startDate cannot be null");
+        if (startDate == null || endDate == null) {
+            throw new IllegalArgumentException("date cannot be null");
         }
 
         try {
             Room availableRoom = roomRepository.findRoomsByBedAmount(bedAmount)
                     .stream()
-                    .filter(room -> isRoomAvailableOnStartDay(room, startDate))
-                    .filter(room -> isRoomAvailable(room, endDate))
+                    .filter(room -> isAvailable(room, startDate, endDate))
                     .findFirst()
-                    .orElseThrow(() -> new NoSuchElementException("No room available on the day: " + startDate));
+                    .orElseThrow(() -> new NoSuchElementException("No room available on the day: " + startDate + " to:" + endDate));
 
             return RoomMapper.entityToDTO(availableRoom);
         } catch (Exception e) {
@@ -50,18 +51,24 @@ class RoomServiceImpl implements RoomService {
         }
     }
 
-    private boolean isRoomAvailable(Room room, LocalDateTime endDate) {
-        return room.getReservations() == null || room.getReservations()
-                .stream()
-                .allMatch(reservation -> reservation.getEndReservation() == null || reservation.getEndReservation().isBefore(endDate));
-    }
+    private boolean isAvailable(Room room, LocalDateTime startDate, LocalDateTime endDate) {
 
-    private boolean isRoomAvailableOnStartDay(Room room, LocalDateTime startDate) {
-        if (room.getReservations() == null) {
+        List<Reservation> reservations = room.getReservations();
+        if (reservations == null || reservations.isEmpty()) {
             return true;
         }
-        return room.getReservations()
+        final boolean isStartDayAvailable = reservations
                 .stream()
                 .noneMatch(r -> r.getStartReservation().equals(startDate));
+
+        if(!isStartDayAvailable){
+            return false;
+        }
+
+        return reservations
+                .stream()
+                .noneMatch(reservation ->
+                        reservation.getStartReservation().isAfter(startDate) &&
+                                reservation.getEndReservation().isBefore(endDate));
     }
 }
