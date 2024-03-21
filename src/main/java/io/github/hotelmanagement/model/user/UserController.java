abf6375058e3b8861317d911b98bccaea1be52d8
mxkooo <106533911+mxkooo@users.noreply.github.com>
2024-03-21 17:51:22 +0100

    User controller (#39)
     Created test

--- src/main/java/io/github/hotelmanagement/model/user/UserController.java
+++ src/main/java/io/github/hotelmanagement/model/user/UserController.java
@@ -2,14 +2,12 @@ package io.github.hotelmanagement.model.user;
 
 
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
 
 @RestController
 @RequestMapping(RoutesUser.ROOT)
--- src/main/java/io/github/hotelmanagement/model/user/UserRepository.java
+++ src/main/java/io/github/hotelmanagement/model/user/UserRepository.java
@@ -1,5 +1,14 @@
 package io.github.hotelmanagement.model.user;
 
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Override
+    boolean existsById(Long aLong);
+
+    @Override
+    <S extends User> S save(S entity);
+
+    @Override
+    <S extends User> boolean exists(Example<S> example);
 }
