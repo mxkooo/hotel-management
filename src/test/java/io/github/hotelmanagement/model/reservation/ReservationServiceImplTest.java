1beb5cfd261b873c387d77b96f0e30a0c7ea37af
PatykKon <115705372+PatykKon@users.noreply.github.com>
2024-04-05 20:06:06 +0200

    Package reservation tests (#49)
    * ReservationServiceImpl tests

--- src/test/java/io/github/hotelmanagement/model/reservation/ReservationServiceImplTest.java
+++ src/test/java/io/github/hotelmanagement/model/reservation/ReservationServiceImplTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -25,6 +26,9 @@ class ReservationServiceImplTest {
     private final static LocalDateTime START_RESERVATION = LocalDateTime.of(2024, 10, 10, 0, 0);
     private final static LocalDateTime END_RESERVATION = LocalDateTime.of(2024, 10, 15, 0, 0);
     private final static int BED_AMOUNT = 2;
+    static ReservationRequest reservationRequest = new ReservationRequest(START_RESERVATION, END_RESERVATION, BED_AMOUNT);
+    static Room room = new Room(1L, 260, BED_AMOUNT, 4, false, new ArrayList<>());
+    static User user = new User(1L, "Jan", "Nowak", new ArrayList<>());
     @BeforeEach
     void prepare(){
         cancelReservationValidator = mock(CancelReservationValidator.class);
@@ -37,17 +41,11 @@ class ReservationServiceImplTest {
     @Test
     void createReservation() {
         //given
-        ReservationRequest reservationRequest = new ReservationRequest(START_RESERVATION, END_RESERVATION, BED_AMOUNT);
-        Room room = new Room(1L, 260, BED_AMOUNT, 4, false, new ArrayList<>());
-        User user = new User(1L, "Jan", "Nowak", new ArrayList<>());
         Reservation reservation = new Reservation(1L, START_RESERVATION, END_RESERVATION, false, room, user);
-
-       //when
+        //when
         when(roomService.getAvailableRoom(START_RESERVATION, END_RESERVATION, BED_AMOUNT)).thenReturn(room);
         when(userService.getUser(1L)).thenReturn(user);
         when(reservationRepository.save(any())).thenReturn(reservation);
-
-
 
         ReservationDTO result = reservationService.createReservation(reservationRequest, 1L);
         //then
@@ -57,5 +55,28 @@ class ReservationServiceImplTest {
         assertNotNull(result.roomDTO());
         assertNotNull(result.userDTO());
         assertNotNull(result);
+    }
+
+    @Test
+    void getAllUserReservation(){
+        Reservation reservation1 = new Reservation(1L, START_RESERVATION, END_RESERVATION, false, room, user);
+        Reservation reservation2 = new Reservation(2L, LocalDateTime.of(2024, 1, 12, 0, 0), LocalDateTime.of(2024, 1, 17, 0, 0), false, room, user);
+        //given
+        List<Reservation> userReservations = new ArrayList<>();
+        userReservations.add(reservation1);
+        userReservations.add(reservation2);
+        //when
+        when(reservationRepository.getReservationByUserId(1L)).thenReturn(userReservations);
+        List<ReservationDTO> result = reservationService.getAllUserReservation(1L);
+
+        //then
+        assertNotNull(result);
+        assertEquals(userReservations.size(), result.size());
+
+        for (int i = 0; i < userReservations.size(); i++) {
+            assertEquals(userReservations.get(i).getId(), result.get(i).id());
+        }
+
+
     }
 }
