98b883e784c6ca0e5e0100a04c3160729e501e8c
mxkooo <106533911+mxkooo@users.noreply.github.com>
2024-04-28 14:38:00 +0200

    Feature update room (#63)

--- src/main/java/io/github/hotelmanagement/model/room/RoomController.java
+++ src/main/java/io/github/hotelmanagement/model/room/RoomController.java
@@ -1,8 +1,9 @@
 package io.github.hotelmanagement.model.room;
 
-import io.github.hotelmanagement.model.user.UserDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @AllArgsConstructor
@@ -18,5 +19,9 @@ public class RoomController {
     @DeleteMapping(RoutesRoom.DELETE + "/{roomId}")
     void deleteRoom(@PathVariable Long roomId) throws Exception{
         roomService.deleteById(roomId);
+    }
+    @PutMapping(RoutesRoom.UPDATE + "/{roomId}")
+    RoomDTO updateRoom(@PathVariable Long roomId, @RequestBody @Valid RoomDTO toUpdate){
+        return roomService.updateRoom(roomId, toUpdate);
     }
 }
