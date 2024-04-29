3abb809d74163020b10b84319362ab9eda42b2e5
mxkooo <106533911+mxkooo@users.noreply.github.com>
2024-04-29 12:13:10 +0200

    Feature get room (#62)

--- src/main/java/io/github/hotelmanagement/model/room/RoomRepository.java
+++ src/main/java/io/github/hotelmanagement/model/room/RoomRepository.java
@@ -1,5 +1,7 @@
 package io.github.hotelmanagement.model.room;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,7 +11,6 @@ public interface RoomRepository extends JpaRepository<Room, Long> {
     List<Room> findRoomsByBedAmount(int bedAmount);
     @Override
     <S extends Room> S save(S entity);
-
     @Override
     void deleteById(Long aLong);
 
@@ -20,5 +21,7 @@ public interface RoomRepository extends JpaRepository<Room, Long> {
     void delete(Room entity);
 
     @Override
-    Optional<Room> findById(Long aLong);
+    List<Room> findAll();
+
+
 }
