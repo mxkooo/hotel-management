3bc6fa926c3e605b2eb45fdcd254da5f0058abe8
mxkooo <106533911+mxkooo@users.noreply.github.com>
2024-04-29 12:09:50 +0200

    Feature update user (#64)

--- src/main/java/io/github/hotelmanagement/model/user/UserController.java
+++ src/main/java/io/github/hotelmanagement/model/user/UserController.java
@@ -6,23 +6,27 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
-
 
 @RestController
 @RequestMapping(RoutesUser.ROOT)
 @AllArgsConstructor
 public class UserController {
-    private UserServiceImpl userService;
+    private UserService userService;
 
     @PostMapping(RoutesUser.POST)
     public UserDTO createUser(@RequestBody @Validated UserDTO userDTO){
         return userService.createUser(userDTO);
     }
+
+    @PutMapping(RoutesUser.UPDATE + "/{userId}")
+    public UserDTO updateUser(@PathVariable Long userId, @RequestBody @Validated UserDTO toUpdate){
+        return userService.updateUser(userId, toUpdate);
+
     @GetMapping(RoutesUser.GET + "/all")
     public List<UserDTO> getAllUsers(){
         return userService.getAllUsers();
     }
+  
     @GetMapping(RoutesUser.GET + "/{userId}")
     public UserDTO findById(@PathVariable Long userId){
         return userService.findUserById(userId);
